363e49d05887d7223980e0df071158c0f5aace96
h01m3s <lin426797@gmail.com>
2018-12-20 00:06:19 -0500

    Rotational Transformation and Card Dismiss

--- SwipeMatch/Views/CardView.swift
+++ SwipeMatch/Views/CardView.swift
@@ -11,6 +11,8 @@ import UIKit
 class CardView: UIView {
     
     fileprivate let imageView = UIImageView(image: #imageLiteral(resourceName: "lady5c"))
+    
+    fileprivate let threshold: CGFloat = 100
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -25,12 +27,11 @@ class CardView: UIView {
     }
 
     @objc fileprivate func handlePan(gesture: UIPanGestureRecognizer) {
-
         switch gesture.state {
         case .changed:
             handlePanGestureStateChanged(gesture)
         case .ended:
-            handlePanGestureEnded()
+            handlePanGestureEnded(gesture)
         default:
             ()
         }
@@ -38,13 +39,34 @@ class CardView: UIView {
     
     fileprivate func handlePanGestureStateChanged(_ gesture: UIPanGestureRecognizer) {
         let translation = gesture.translation(in: nil)
-        self.transform = CGAffineTransform(translationX: translation.x, y: translation.y)
+        // Rotation
+        // convert radians to degrees
+        let degrees: CGFloat = translation.x / 20
+        let angle = degrees * .pi / 180
+        let rotationalTransformation = CGAffineTransform(rotationAngle: angle)
+        self.transform = rotationalTransformation.translatedBy(x: translation.x, y: translation.y)
     }
     
-    fileprivate func handlePanGestureEnded() {
+    fileprivate func handlePanGestureEnded(_ gesture: UIPanGestureRecognizer) {
+        let farAwayX: CGFloat = gesture.translation(in: nil).x > 0 ? 1000.0 : -1000.0
+        let shouldDismissCard = abs(gesture.translation(in: nil).x) > threshold
+
         UIView.animate(withDuration: 0.75, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0.1, options: .curveEaseOut, animations: {
+            
+            if (shouldDismissCard ) {
+                
+                self.frame = CGRect(x: farAwayX, y: 0, width: self.frame.width, height: self.frame.height)
+
+            } else {
+                self.transform = .identity
+            }
+            
+        }) { (_) in
+            
             self.transform = .identity
-        }, completion: nil)
+            self.frame = CGRect(x: 0, y: 0, width: self.superview!.frame.width, height: self.superview!.frame.height)
+            
+        }
     }
     
     required init?(coder aDecoder: NSCoder) {
