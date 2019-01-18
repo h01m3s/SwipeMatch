f39b226509d657840f2187df15071a3a5fab9ec5
h01m3s <lin426797@gmail.com>
2019-01-17 23:09:28 -0500

    Advertiser Model Protocol Oriented Programming Partial

---
+++ SwipeMatch/Model/Advertiser.swift
@@ -0,0 +1,23 @@
+//
+//  Advertiser.swift
+//  SwipeMatch
+//
+//  Created by Weijie Lin on 1/16/19.
+//  Copyright © 2019 Weijie Lin. All rights reserved.
+//
+
+import UIKit
+
+struct Advertiser {
+    
+    let title: String
+    let brandName: String
+    let posterPhotoName: String
+    
+    func toCardViewModel() -> CardViewModel {
+        let attributedString = NSMutableAttributedString(string: title, attributes: [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 34, weight: .heavy)])
+        attributedString.append(NSAttributedString(string: "\n\(brandName)", attributes: [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 24, weight: .bold)]))
+        return CardViewModel(imageName: posterPhotoName, attributedString: attributedString, textAlignment: .center)
+    }
+    
+}
